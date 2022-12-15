f7b7f724c51a351dc7572679fb7ffd678f918cb8
Ashish's ASUS laptop <hrushikeshmohanty90@gmail.com>
2022-12-15 01:08:44 -0500

    minor updates in style

--- src/components/communication/components/MessageDialog.js
+++ src/components/communication/components/MessageDialog.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import TextField from '@mui/material/TextField';
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
@@ -8,28 +10,33 @@ import DialogTitle from '@mui/material/DialogTitle';
 import { Button } from '@mui/material';
 import TextareaAutosize from '@mui/base/TextareaAutosize';
 import emailjs from '@emailjs/browser';
+import Alert from '@mui/material/Alert';;
 
 const MessageDialog = ({ open, setOpen, patientDetails }) => {
     const [message, setMessage] = useState('')
+    const [successAlert, setSuccessAlert] = useState(false)
+    const [errorAlert, setErrorAlert] = useState(false)
+    const [warning, setWarning] = useState(false)
 
     const handleSend = (e) => {
         e.preventDefault()
         console.log(message)
         if (message === "") {
-            window.alert('no message has been typed')
+            setOpen(false)
+            setWarning(true)
             return
         }
 
-        console.log({user_name: patientDetails.firstName+' '+patientDetails.lastName, message})
-        emailjs.send('service_9cc48op', 'template_vzloy0h', {user_name: patientDetails.firstName+' '+patientDetails.lastName, message}, 'dKhTusOMbDiFUYTOp')
+        console.log({ user_name: patientDetails.firstName + ' ' + patientDetails.lastName, message })
+        emailjs.send('service_9cc48op', 'template_vzloy0h', { user_name: patientDetails.firstName + ' ' + patientDetails.lastName, message }, 'dKhTusOMbDiFUYTOp')
             .then((result) => {
                 console.log(result.text);
-                window.alert('Message has been sent')
                 handleClose()
+                setSuccessAlert(true)
             }, (error) => {
                 console.log(error.text);
-                window.alert('Error in sending message. Please try again later')
                 handleClose()
+                setErrorAlert(true)
             });
 
     }
@@ -54,6 +61,45 @@ const MessageDialog = ({ open, setOpen, patientDetails }) => {
                     <Button onClick={handleSend}>Send</Button>
                 </DialogActions>
             </Dialog>
+            {successAlert ? 
+            <Alert severity="success" sx={{ mb: 2 }} action={
+                <IconButton
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={() => {
+                        setSuccessAlert(false);
+                    }}
+                >
+                    <CloseIcon fontSize="inherit" />
+                </IconButton>
+            }> Message has been sent</Alert> : <></>}
+            {errorAlert ? 
+            <Alert severity="error" sx={{ mb: 2 }} action={
+                <IconButton
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={() => {
+                        setErrorAlert(false);
+                    }}
+                >
+                    <CloseIcon fontSize="inherit" />
+                </IconButton>
+            }> There was an error, check it out</Alert> : <></>}
+            {warning ? 
+            <Alert severity="warning" sx={{ mb: 2 }} action={
+                <IconButton
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={() => {
+                        setWarning(false);
+                    }}
+                >
+                    <CloseIcon fontSize="inherit" />
+                </IconButton>
+            }> Warning! No empty message allowed</Alert> : <></>}
         </div>
     )
 }
--- src/components/communication/components/RefillDialog.js
+++ src/components/communication/components/RefillDialog.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import Alert from '@mui/material/Alert';
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
 import { Button } from '@mui/material';
 import emailjs from '@emailjs/browser';
 
 const RefillDialog = ({setOpen, open, patientDetails}) => {
+    const [successAlert, setSuccessAlert] = useState(false)
+    const [errorAlert, setErrorAlert] = useState(false)
 
     const handleClose = () => {
         setOpen(false);
@@ -17,12 +22,18 @@ const RefillDialog = ({setOpen, open, patientDetails}) => {
         emailjs.send('service_9cc48op', 'template_zvptpoo', {user_name: patientDetails.firstName+' '+patientDetails.lastName, message: "Please approve a refill of my current medication"}, 'dKhTusOMbDiFUYTOp')
             .then((result) => {
                 console.log(result.text);
-                window.alert('Request has been sent')
                 handleClose()
+                setSuccessAlert(true)
+                setTimeout(() => {
+                    setSuccessAlert(false)                                        
+                }, 6000);
             }, (error) => {
                 console.log(error.text);
-                window.alert('Error in sending request. Please try again later')
                 handleClose()
+                setErrorAlert(true)
+                setTimeout(() => {
+                    setErrorAlert(false)                                        
+                }, 6000);
             });
       }
         
@@ -40,6 +51,32 @@ const RefillDialog = ({setOpen, open, patientDetails}) => {
           <Button onClick={handleConfirmRequest}>Confirm Request</Button>
         </DialogActions>
       </Dialog>
+      {successAlert ? 
+            <Alert severity="success" sx={{ mb: 2 }} action={
+                <IconButton
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={() => {
+                        setSuccessAlert(false);
+                    }}
+                >
+                    <CloseIcon fontSize="inherit" />
+                </IconButton>
+            }> Request has been sent</Alert> : <></>}
+            {errorAlert ? 
+            <Alert severity="error" sx={{ mb: 2 }} action={
+                <IconButton
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={() => {
+                        setErrorAlert(false);
+                    }}
+                >
+                    <CloseIcon fontSize="inherit" />
+                </IconButton>
+            }> There was an error, check it out</Alert> : <></>}
     </div>
   )
 }
